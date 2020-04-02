bf8e3f5676437de4dc3cc95f65a32f519e4bd2dd
heyongqi <heyongqi@isafetyworld.com>
2020-04-02 15:42:05 +0800

    hyqsm19931992,提交controller

--- src/main/java/com/git/demo/controller/HelloController.java
+++ src/main/java/com/git/demo/controller/HelloController.java
@@ -1,12 +1,10 @@
 package com.git.demo.controller;
 
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@ResponseBody
-@Controller
+@RestController
 public class HelloController {
 
     @RequestMapping("/hello")
