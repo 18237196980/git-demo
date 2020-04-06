bd126e02468fe892082466d943f2171ec73dae67
18237196980 <heyongqi@isafetyworld.com>
2020-04-06 10:04:30 +0800

    日志配置文件

--- src/main/java/com/git/demo/controller/HelloController.java
+++ src/main/java/com/git/demo/controller/HelloController.java
@@ -1,14 +1,25 @@
 package com.git.demo.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping("/hello")
     public Object hello() {
+
+        logger.trace("trace...");
+        logger.debug("debug...");
+        logger.info("info...");
+        logger.warn("warn...");
+        logger.error("error...");
+
         int a = 97;
         return "hello hyq";
     }
